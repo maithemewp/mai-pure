75ce2d44f60ff7e3a341eca350e8d78e05d99210
Mike Hemberger <mike@thestizmedia.com>
2022-03-11 14:43:05 -0500

    Initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'maithemewp/mai-pure',
+  ),
+  'versions' => 
+  array (
+    'afragen/wp-dependency-installer' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'maithemewp/mai-installer' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '399fec687a9459b774c1bd1c44a38a806dfa052f',
+    ),
+    'maithemewp/mai-pure' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
